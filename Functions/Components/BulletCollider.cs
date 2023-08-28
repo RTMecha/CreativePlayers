050d091b423eba7dc92d006a0b0381c2f73130dd
RTMecha <mechaprower2022@gmail.com>
2023-08-28 21:00:02 +1000

    Bullet AutoKill

---
+++ Functions/Components/BulletCollider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using UnityEngine;
+
+using DG.Tweening;
+
+namespace CreativePlayers.Functions.Components
+{
+    public class BulletCollider : MonoBehaviour
+    {
+        public RTPlayer.PlayerObject playerObject;
+        public RTPlayer player;
+        public Rigidbody2D rb;
+        public bool kill = false;
+        public Tweener tweener;
+
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.transform.parent.parent.name != player.name && kill)
+            {
+                tweener.Kill();
+
+                player.boosts.Remove(playerObject);
+                playerObject = null;
+                Destroy(transform.parent.gameObject);
+            }
+        }
+    }
+}
